d8c49d18bdbebe2c514c34e6ba760d72b8d35bff
craiglobo1 <craiglobo1@gmail.com>
2023-01-14 18:14:33 -0700

    added usage for readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-"# BallChasing" 
+# BallChasing - Replay Auto Downloader
+---
+
+
+## Usage
+You need your ballchasing api key as an enviromental variable
+```bash
+set BALLCHASING-API=<your-ballchasing-api-key>
+```
+Then run main.py
+```python
+python main.py
+```
--- main.py
+++ main.py
@@ -3,29 +3,39 @@ import os
 import json
 import time
 
-MAX_COUNT = 200  # max count for replays defined in api docs
+MAX_COUNT = 10  # max count for replays defined in api docs
 
 auth_token = os.getenv("BALLCHASING-API")
 
 def parse_url_params(params : dict):
     return "?" + "&".join([ f"{key}={val}" for key, val in params.items()])
 
+done_ids = set([ file[:-7] for file in os.listdir("replays")])
+
 replays_params = {
     "count": MAX_COUNT, 
 }
 
 replays = requests.get(f"https://ballchasing.com/api/replays{parse_url_params(replays_params)}", headers={"Authorization": auth_token}).json()
-# replays = json.load(open("example_req.json", "r"))
-
 ids = [ replay["id"] for replay in replays["list"]]
+
+# removes ids that have been done
+ids = list(set(ids)-done_ids)
+print(f"[NEW] Replenishing {len(ids)}")
 
 start = time.time()
 
 i = 0
+
 while True:
     if i >= len(ids):
-        print("hey")
-        break
+        new_replays = requests.get(f"https://ballchasing.com/api/replays{parse_url_params(replays_params)}", headers={"Authorization": auth_token}).json()
+        new_ids = [ replay["id"] for replay in replays["list"]]
+        # removes ids that have been done
+        ids = list((set(new_ids)-set(ids))-done_ids)
+        print(f"[NEW] Replenishing {len(ids)}")
+        i = 0
+
     req = requests.get(f"https://ballchasing.com/api/replays/{ids[i]}/file", headers={"Authorization": auth_token})
 
     with open(f"replays/{ids[i]}.replay", "wb+") as wf:
@@ -35,9 +45,8 @@ while True:
         print(f"id_{i}: {time.time()-start:.4f}")
         start = time.time()
     elif req.status_code == 429:
-        print(f"ERROR: Timeout ")
-        print(req.json())
+        print(f"ERROR: {req.json()['error']}")
     else:
-        print(req.json())
+        print(f"ERROR: {req.json()['error']}")
 
     i += 1
